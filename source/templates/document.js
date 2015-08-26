678395ac013c4de14ccd9b723ec1f722a12e5f1b
Kyle Mitchell <kyle@kemitchell.com>
2015-08-25 23:26:52 -0700

    Upgrade to commonform-flatten@0.6.x

--- source/templates/document.js
+++ source/templates/document.js
@@ -1,9 +1,10 @@
+var clone = require('clone');
 var decimalStyle = require('decimal-numbering');
 var flatten = require('commonform-flatten');
 var paragraph = require('./paragraph');
 
 module.exports = function(form, values) {
-  return flatten(form, values).map(function(element) {
+  return flatten(clone(form), values).map(function(element) {
     return paragraph(element, decimalStyle);
   }).join('');
 };
