87502f9f4ae1c0c06f2735909d5ab71e1ac0c0f7
Kyle Mitchell <kyle@kemitchell.com>
2015-08-25 23:28:48 -0700

    Do not clone argument to commonform-flatten

--- source/templates/document.js
+++ source/templates/document.js
@@ -1,10 +1,9 @@
-var clone = require('clone');
 var decimalStyle = require('decimal-numbering');
 var flatten = require('commonform-flatten');
 var paragraph = require('./paragraph');
 
 module.exports = function(form, values) {
-  return flatten(clone(form), values).map(function(element) {
+  return flatten(form, values).map(function(element) {
     return paragraph(element, decimalStyle);
   }).join('');
 };
